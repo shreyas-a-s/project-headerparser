7e881fa5b6eee0a06600e6e25893d122d175b714
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-19 18:32:39 +0530

    feat: Complete index.js with functionality

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+app.get("/api/whoami", function (req, res) {
+  const ip = req.ip;
+  // console.log(JSON.stringify(req.headers));
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.send({ip: ip, language: language, software: software});
+})
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
